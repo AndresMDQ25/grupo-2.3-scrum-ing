ea208c1f1a0a9a9751471f5316c08436cd74af00
Bruno Rios <Bruno Rios>
2016-06-21 20:17:57 -0300

    Revert "Un detalle cambie nomas, import expotar por import expoter"
    This reverts commit 4b71098ff29bded07ddb0db2ddc336b473143931.

--- src/com/scrum23/exporter/Draw_Graph.java
+++ src/com/scrum23/exporter/Draw_Graph.java
@@ -1,4 +1,4 @@
-package exporter;
+package exportar;
 
 import java.awt.Desktop;
 import java.io.File;
@@ -6,6 +6,7 @@ import java.io.IOException;
 
 public class Draw_Graph {
 	public static void main(String[] args) {
+		try {
 			File dir_actual = new File(".");
 			try {
 				String dir = dir_actual.getCanonicalPath();
@@ -48,5 +49,9 @@ public class Draw_Graph {
 			       e.printStackTrace();
 			}
 
+		    
+		} catch (Exception ex) {
+		      ex.printStackTrace();
+	    } 	
 	}
 }
